fa4780d8ef7b23434f55af23b5e588fa3801d2c8
Zaman <zamanofficial1980@gmail.com>
2024-04-07 02:59:23 +0530

    commit some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import { useState, useCallback, useEffect } from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
 
+// ---useState hook
 function App() {
   const [length, setLength] = useState()
   const [addNumber, setAddumber] = useState(false)
   const [addCharacters, setAddcharacters] = useState(false)
   const [password, setPassword] = useState("")
 
+
+//--- useRef hook for refference---
+
+const passwordRef = useRef(null)
+
+//---useCallback hook(for memoization)----
 const passwordGenerator = useCallback(() => {
   let pass = ""
   let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
@@ -22,6 +29,13 @@ const passwordGenerator = useCallback(() => {
 
 },[length,addNumber,addCharacters,setPassword])
 
+const copyPassword = useCallback(()=>{
+      passwordRef.current?.select()
+      // passwordRef.current?.setSelectionRange(0, 2)
+      window.navigator.clipboard.writeText(password)
+
+},[password])
+//--- useEffect hook for UI update----
 useEffect(() => {
   passwordGenerator()
 
@@ -30,16 +44,20 @@ useEffect(() => {
   return (
     <>
    
-      <div className='flex flex-col min-h-screen justify-center items-center w-full h-screen bg-slate-700 text-red    p-5'>
+      <div className='flex flex-col min-h-screen justify-center items-center w-full h-screen bg-slate-900 text-red    p-5'>
         <div className='flex flex-col justify-center items-center bg-slate-600 rounded-md px-4 py-2'>
+          <h1 className='text-white font-bold text-xl py-2 mb-5'>Random Password Generator</h1>
         <div className='w-full'>
-          <input className='rounded-s-md p-3 w-10/12' 
+          <input className='rounded-s-md p-3 w-10/12 outline-none' 
           type="text"
           value={password}
           placeholder='Password'
           readOnly
+          ref={passwordRef}
            />
-          <button className='bg-blue-500 rounded-e-md p-3 text-white'>Copy</button>
+          <button className='bg-blue-500 rounded-e-md p-3 text-white '
+          onClick={copyPassword}
+          >Copy</button>
         </div>
         <div className='flex flex-wrap gap-2 text-orange-500 my-2 font-bold'>
           <input className='cursor-pointer w-auto' 
